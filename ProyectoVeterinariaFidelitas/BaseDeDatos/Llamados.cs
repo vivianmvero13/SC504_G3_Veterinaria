0965c5309c99792de9cf062a8762c39f582b6f83
Josencion <josealberto352@hotmail.com>
2025-03-28 00:35:03 -0600

    ProyectoWeb

---
+++ ProyectoVeterinariaFidelitas/BaseDeDatos/Llamados.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using static BaseDeDatos.ModeloDatos;
+
+namespace BaseDeDatos
+{
+    public class Llamados
+    {
+        public List<Estado> ObtenerEstados()
+        {
+            List<Estado> estados = new List<Estado>();
+            string sqlQuery = "SELECT ID_ESTADO_PK, ESTADO_DESCRIPCION, CREATED_BY, CREATION_DATE, MODIFIED_BY, MODIFICATION_DATE FROM FIDE_ESTADO_TB";
+
+            using (OracleConnection connection = new OracleConnection(ConfigurationManager.ConnectionStrings["TuCadenaDeConexion"].ConnectionString))
+            {
+                connection.Open();
+                using (OracleCommand command = new OracleCommand(sqlQuery, connection))
+                {
+                    using (OracleDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Estado estado = new Estado
+                            {
+                                IdEstadoPk = Convert.ToInt32(reader["ID_ESTADO_PK"]),
+                                EstadoDescripcion = reader["ESTADO_DESCRIPCION"].ToString(),
+                                CreatedBy = reader["CREATED_BY"]?.ToString(),
+                                CreationDate = reader["CREATION_DATE"] == DBNull.Value ? (DateTime?)null : Convert.ToDateTime(reader["CREATION_DATE"]),
+                                ModifiedBy = reader["MODIFIED_BY"]?.ToString(),
+                                ModificationDate = reader["MODIFICATION_DATE"] == DBNull.Value ? (DateTime?)null : Convert.ToDateTime(reader["MODIFICATION_DATE"])
+                            };
+                            estados.Add(estado);
+                        }
+                    }
+                }
+            }
+            return estados;
+        }
+
+        public void InsertarEstado(int idEstado, string estadoDescripcion, string created_by, DateTime creationDate, string modified_by, DateTime modificationDate)
+        {
+            using (OracleConnection connection = new OracleConnection(ConfigurationManager.ConnectionStrings["TuCadenaDeConexion"].ConnectionString))
+            {
+                connection.Open();
+                using (OracleCommand command = new OracleCommand("PROYECTO_FUNCIONES_PROCEDIMIENTOS_PKG.INSERT_ESTADO_TB_SP", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.Add("p_id_estado", OracleDbType.Int32).Value = idEstado;
+                    command.Parameters.Add("p_estado_descripcion", OracleDbType.Varchar2, 100).Value = estadoDescripcion;
+                    command.Parameters.Add("p_created_by", OracleDbType.Varchar2, 50).Value = created_by;
+                    command.Parameters.Add("p_creation_date", OracleDbType.Date).Value = creationDate;
+                    command.Parameters.Add("p_modified_by", OracleDbType.Varchar2, 50).Value = modified_by;
+                    command.Parameters.Add("p_modification_date", OracleDbType.Date).Value = modificationDate;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void ActualizarEstado(int idEstado, string estadoDescripcion, string modified_by, DateTime modificationDate)
+        {
+            using (OracleConnection connection = new OracleConnection(ConfigurationManager.ConnectionStrings["TuCadenaDeConexion"].ConnectionString))
+            {
+                connection.Open();
+                using (OracleCommand command = new OracleCommand("PROYECTO_FUNCIONES_PROCEDIMIENTOS_PKG.UPDATE_ESTADO_TB_SP", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.Add("p_id_estado", OracleDbType.Int32).Value = idEstado;
+                    command.Parameters.Add("p_estado_descripcion", OracleDbType.Varchar2, 100).Value = estadoDescripcion;
+                    command.Parameters.Add("p_modified_by", OracleDbType.Varchar2, 50).Value = modified_by;
+                    command.Parameters.Add("p_modification_date", OracleDbType.Date).Value = modificationDate;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void EliminarEstado(int idEstado)
+        {
+            string sqlQuery = "DELETE FROM FIDE_ESTADO_TB WHERE ID_ESTADO_PK = :IdEstado";
+
+            using (OracleConnection connection = new OracleConnection(ConfigurationManager.ConnectionStrings["TuCadenaDeConexion"].ConnectionString))
+            {
+                connection.Open();
+                using (OracleCommand command = new OracleCommand(sqlQuery, connection))
+                {
+                    // Usar parámetros para evitar inyección SQL
+                    command.Parameters.Add(new OracleParameter("IdEstado", OracleDbType.Int32)).Value = idEstado;
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                }
+            }
+        }
+    }
+}
---
+++ ProyectoVeterinariaFidelitas/Veterinaria/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaseDeDatos;
+using static BaseDeDatos.ModeloDatos;
+
+namespace Veterinaria.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Estado()
+        {
+            return View();
+        }
+
+        public ActionResult ObtenerEstados()
+        {
+            Llamados llamados = new Llamados();
+            List<ModeloDatos.Estado> estados = llamados.ObtenerEstados();
+            return Json(estados, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ActualizarEstado(int id, string estado, string actualizadoPor)
+        {
+            string respuesta = "00";
+            Llamados llamados = new Llamados();
+            llamados.ActualizarEstado(id, estado, actualizadoPor, DateTime.Today);
+            return Json( respuesta , JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult CrearEstado(int id, string estado, string creadoPor)
+        {
+            string respuesta = "00";
+            Llamados llamados = new Llamados();
+            llamados.InsertarEstado(id, estado, creadoPor, DateTime.Today, creadoPor, DateTime.Today);
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult EliminarEstado(int id)
+        {
+            string respuesta = "00";
+            Llamados llamados = new Llamados();
+            llamados.EliminarEstado(id);
+            return Json(respuesta, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
